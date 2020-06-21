e9bf8670f7892e9c6e2844c861d8241c0bdc765e
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-06-21 18:26:18 -0400

    Stage 6 of 8 for JetBrains Academy Kotlin Smart Calculator project. • a mutable map called NUMBERS was added so that the user can store values • for the run() function the math part was moved to the new function doMath() and if user wants to store a value the new function memoryAdd() is called. • doMath() does the addition and subtraction of values, and calls the new function memoryGet() if the user is referring to a stored value. • memoryADD() will store a number with a given name if the name the user gives it contains all Latin letters and the numbers to be stored is either all numbers or the name of an already stored value. • memoryGet() will return the number of a stored value if it is found, and if it isn't it will return null. • unknown() function was renamed unknownCMD() • unknownVar() function was created for unknown variables • invalid() function was renamed invalidExp() • invalidAssign() was created for invalid assignment • invalidID() was created for invalid identifier

--- src/SmartCalculator.kt
+++ src/SmartCalculator.kt
@@ -1,6 +1,8 @@
 import java.util.*
 
 object SmartCalculator {
+    private val NUMBERS = mutableMapOf<String, Int>()
+
     fun run() {
         val scanner = Scanner(System.`in`)
         var input = scanner.nextLine()
@@ -8,35 +10,7 @@ object SmartCalculator {
         while (input != "/exit") {
             if (input != "") {
                 if (input[0] != '/') {
-                    val strings = input.split(" ").toTypedArray()
-                    var sum = 0
-                    var shouldBeNumber = true
-                    var subtract = false
-                    var print = true
-
-                    for (num in strings) {
-                        var char = num[0]
-                        if (num.length > 1 && (char == '-' || char == '+') && char != num[1]) char = ' '
-
-                        if (shouldBeNumber) {
-                            val num1 = if (isNumber(num)) num.toInt() else {
-                                print = false
-                                continue
-                            }
-                            sum += if (subtract) -num1 else num1
-                            if (subtract) subtract = false
-                            shouldBeNumber = false
-                        } else {
-                            if (char == '-' || char == '+') {
-                                if (char == '-') for (char2 in num) if (char2 == '-') subtract = !subtract
-                                shouldBeNumber = true
-                            } else {
-                                print = false
-                                continue
-                            }
-                        }
-                    }
-                    if (print) println(sum) else invalid()
+                    if (input.contains('=')) memoryAdd(input) else doMath(input)
                 } else command(input)
             }
             input = scanner.nextLine()
@@ -44,7 +18,60 @@ object SmartCalculator {
         println("Bye!")
     }
 
-    private fun command(command: String) = if (command == "/help") help() else unknown()
+    private fun doMath(expression: String) {
+        val strings = expression.split(" ").toTypedArray()
+        var sum = 0
+        var shouldBeNumber = true
+        var subtract = false
+        var print = true
+
+        for (num in strings) {
+            var char = num[0]
+            if (num.length > 1 && (char == '-' || char == '+') && char != num[1]) char = ' '
+
+            if (shouldBeNumber) {
+                val num1: Int? = if (isNumber(num)) num.toInt() else memoryGet(num)
+                if (num1 == null) {
+                    print = false
+                    unknownVar()
+                    continue
+                }
+                sum += if (subtract) -num1 else num1
+                if (subtract) subtract = false
+                shouldBeNumber = false
+            } else {
+                if (char == '-' || char == '+') {
+                    if (char == '-') for (char2 in num) if (char2 == '-') subtract = !subtract
+                    shouldBeNumber = true
+                } else {
+                    print = false
+                    invalidExp()
+                    continue
+                }
+            }
+        }
+        if (print) println(sum)
+    }
+
+    private fun memoryAdd(value: String) {
+        val values = value.replace(" ", "").split('=').toTypedArray()
+        val sequence: CharRange = 'a'..'z'
+        if (values.size > 2) invalidAssign() else {
+            for (char in values[0]) if (!sequence.contains(char.toLowerCase())) {
+                invalidID()
+                return
+            }
+            when {
+                isNumber(values[1]) -> NUMBERS[values[0]] = values[1].toInt()
+                memoryGet(values[1]) != null -> NUMBERS[values[0]] = memoryGet(values[1]) ?: 0
+                else -> invalidAssign()
+            }
+        }
+    }
+
+    private fun memoryGet(value: String): Int? = if (NUMBERS.containsKey(value)) NUMBERS[value] else null
+
+    private fun command(command: String) = if (command == "/help") help() else unknownCMD()
 
     private fun help() {
         println(
@@ -53,9 +80,15 @@ object SmartCalculator {
         )
     }
 
-    private fun unknown() = println("Unknown command")
+    private fun unknownCMD() = println("Unknown command")
 
-    private fun invalid() = println("Invalid expression")
+    private fun unknownVar() = println("Unknown variable")
+
+    private fun invalidExp() = println("Invalid expression")
+
+    private fun invalidAssign() = println("Invalid assignment")
+
+    private fun invalidID() = println("Invalid identifier")
 
     private fun isNumber(number: String) = number.toIntOrNull() != null
 }
